8202d31dfeeb8ce9c4d1c05c7289e23454879adb
VSI12 <victoriliya15@gmail.om>
2024-12-27 23:06:20 +0100

    temporarily added the upload component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import UploadDataset from "../components/upload-dataset";
 
 export default function Home() {
   return (
     <div>
-
+      <UploadDataset />
     </div>
   );
 }
