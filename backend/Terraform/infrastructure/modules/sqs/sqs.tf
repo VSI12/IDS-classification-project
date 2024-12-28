b062e9f6d75195d43190d55744bd6fd7cbf088f9
VSI12 <victoriliya15@gmail.om>
2024-12-28 09:10:03 +0100

    Added the IAM policy to allow the S3 bucket

--- backend/Terraform/infrastructure/modules/sqs/sqs.tf
+++ backend/Terraform/infrastructure/modules/sqs/sqs.tf
@@ -1,3 +1,23 @@
+data "aws_iam_policy_document" "queue" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = ["arn:aws:sqs:*:*:s3-event-notification-queue"]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.bucket.arn]
+    }
+  }
+}
 resource "aws_sqs_queue" "file_upload_queue" {
   name = var.queue_name
+  policy = data.aws_iam_policy_document.queue.json
 }
