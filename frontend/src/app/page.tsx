743112c08b1b6b4383bb5628dcd3ffb3148d1ad8
VSI12 <victoriliya15@gmail.om>
2024-12-28 16:59:12 +0100

    form

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import UploadDataset from "../components/upload-dataset";
+import ModelSelectionForm from "@/components/models";
 
 export default function Home() {
   return (
     <div>
       <UploadDataset />
+      <ModelSelectionForm/>
     </div>
   );
 }
