f461db75b605f7b0ca84ef8ddbdbfe90c4cf2414
VSI12 <victoriliya15@gmail.om>
2024-12-28 09:40:13 +0100

    out

--- backend/Terraform/infrastructure/variables.tf
+++ backend/Terraform/infrastructure/variables.tf
@@ -1,10 +1,10 @@
-variable "upload_bucket_name" {
-  description = "The name of the S3 bucket to store the uploaded files."
-  type        = string
+# variable "upload_bucket_name" {
+#   description = "The name of the S3 bucket to store the uploaded files."
+#   type        = string
   
-}
+# }
 
-variable "queue_name" {
-  description = "The name of the SQS queue"
-  type        = string
-}
+# variable "queue_name" {
+#   description = "The name of the SQS queue"
+#   type        = string
+# }
